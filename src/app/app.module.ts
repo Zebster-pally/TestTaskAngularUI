ace5a9acca5a9f0666e7c805fed6d127b3085ac2
zebsterino <velikolepniy65@gmail.com>
2021-01-11 20:24:11 +0200

    added components; added animation for open search input in header;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialModule } from './material.module';
 import { SearchDirective } from './search.directive';
+import { ProfileComponent } from './profile/profile.component';
+import { InputDropdownComponent } from './input-dropdown/input-dropdown.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SearchDirective
+    SearchDirective,
+    ProfileComponent,
+    InputDropdownComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
